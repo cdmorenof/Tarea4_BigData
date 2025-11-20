cf7d077d0797970f6f69332236ea8950ce92ac04
cdmorenof <151644605+cdmorenof@users.noreply.github.com>
2025-11-19 23:27:34 -0500

    Insert patient record for Laura Herrera

---
+++ 🔹 01_insercion.js
@@ -0,0 +1,14 @@
+db.historias_clinicas.insertOne({
+  "paciente_id": "P102",
+  "nombre": "Laura Herrera",
+  "edad": 38,
+  "sexo": "Femenino",
+  "diagnosticos": "Migraña crónica",
+  "medicamento_1": "Sumatriptán",
+  "dosis_1": "100mg",
+  "frec_1": "Cuando se presente dolor",
+  "medicamento_2": "Naproxeno",
+  "dosis_2": "250mg",
+  "frec_2": "2 veces al día",
+  "fecha_ultima_visita": "05/11/2025"
+});
